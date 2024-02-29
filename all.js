b8577ca54ab6863bccbe1e8888d7edc9369e8c2c
chenjiayin <chen19970221@gmail.com>
2024-02-29 13:04:26 +0800

    0229 update

--- all.js
+++ all.js
@@ -7,19 +7,23 @@ const thing = document.querySelector('.thing')
 const filterBtn = document.querySelector('.tab')
 const deleteBtn = document.querySelector('.delete') 
 const checkBtn = document.querySelector('.checkBtn')
+const todoThing = document.querySelector('.list li') 
 
 
+
+// render
 let data = []
-
 function render(){
   let str = ""
-  data.forEach(function(item){
+  data.forEach(function(item,index){
     let content = 
-    `<li><label class="checkbox" for="">
-    <input class="checkBtn" type="checkbox" />
-    <span>${item.content}</span>
-    </label>
-    <a href="#" class="delete"></a></li>`
+    `<li  data-thing="${index}">
+      <label class="checkbox  for="">
+        <input class="checkBtn" type="checkbox" />
+        <span>${item.content}</span>
+      </label>
+      <a href="#" class="delete"></a>
+    </li>`
     str += content
   })
   list.innerHTML = str
@@ -44,47 +48,64 @@ btn_add.addEventListener('click', function(e){
 
 // 進度分類
 filterBtn.addEventListener('click', function(e){
-  console.log(e.target.getAttribute('class'))
+  if(e.target.className == "tab"){
+    return
+  }
+
+  
   let str = ""
-  data.forEach(function(item){
+  data.forEach(function(item,index){
     if (e.target.getAttribute('class')=="active") {
       str +=  
-      `<li><label class="checkbox" for="">
-      <input chenk="checkBtn" type="checkbox" />
-      <span>${item.content}</span>
-      </label>
-      <a href="#" class="delete"></a></li>`
+      `<li  data-thing="${index}">
+        <label class="checkbox  for="">
+          <input class="checkBtn" type="checkbox" />
+          <span>${item.content}</span>
+        </label>
+        <a href="#" class="delete"></a>
+      </li>`
+      console.log(str)
     }
-   
     if (e.target.getAttribute('class')=="pending") {
       str += 
-      `<li><label class="checkbox" for="">
-      <input chenk="checkBtn" type="checkbox" />
-      <span>${item.content}</span>
+      `<li  data-thing="${index}">
+      <label class="checkbox  for="">
+        <input class="checkBtn" type="checkbox" />
+        <span>${item.content}</span>
       </label>
-      <a href="#" class="delete"></a></li>`
+      <a href="#" class="delete"></a>
+    </li>`
     } 
-
     if (e.target.getAttribute('class')=="complete") {
-      str +=  
-      `<li><label class="checkbox" for="">
-      <input chenk="checkBtn" type="checkbox" />
-      <span>${item.content}</span>
-      </label>
-      <a href="#" class="delete"></a></li>`
+      if (li.getAttribute('class')=="finished") {
+        str +=  
+      `<li data-thing="${index}">
+        <label class="checkbox  for="">
+          <input class="checkBtn" type="checkbox" />
+          <span>${item.content}</span>
+        </label>
+        <a href="#" class="delete"></a>
+      </li>`
+      }
     }
-    
+      list.innerHTML = str
+    })
   })
-  list.innerHTML = str
+    
+
+ 
+
+
+
+
+
+// 已完成頁面：選項賦予一個 class="finished"
+list.addEventListener("click", function(e){
+  if(e.target.nodeName == "INPUT"){
+    const todoThing = document.querySelector('.list li') 
+    todoThing.setAttribute("class", "finished")
+  }
+  
 })
 
-
-
-// 已完成頁面：選項賦予一個 class="complete"
-checkBtn.addEventListener("click", function(e){
-  console.log(e.target)
-})
-
-
-// 待完成頁面：選項賦予一個 class="pending"
 
