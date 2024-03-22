18993c65c9bcf8b18a71a0ee6eff789c7f9406f1
chenjiayin <chen19970221@gmail.com>
2024-03-22 16:14:41 +0800

    fix swtich button

--- all.js
+++ all.js
@@ -2,66 +2,42 @@ const active = document.querySelector('.active');
 const pending = document.querySelector('.pending');
 const complete = document.querySelector('.complete');
 const list = document.querySelector('.list');
-const btn_add = document.querySelector('.btn_add')
+const btnAdd = document.querySelector('.btn_add')
 const thing = document.querySelector('.thing')
 const filterBtn = document.querySelector('.tab')
-const list_footer = document.querySelector('.list_footer')  
-
-
+const listFooter = document.querySelector('.list_footer')  
 
 
 // 初始 render
 let data = []
 
-function render(status = 'active'){
-  let str = ""
-  data.forEach(function(item,index){
-    if (status == "active"){
-    let content =
-    `<li class="${item.status}">
-      <label class="checkbox  for="">
-        <input class="checkBtn" type="checkbox" data-thing="${index}"/>
-        <span>${item.content}</span>
-      </label>
-      <a href="#" class="delete" data-thing="${index}"></a>
-    </li>`
-    str += content
-  } else{
-    if (item.status == status){
-      let content =
-      `<li class="${item.status}">
-        <label class="checkbox  for="">
-          <input class="checkBtn" type="checkbox" data-thing="${index}"/>
-          <span>${item.content}</span>
-        </label>
-        <a href="#" class="delete" data-thing="${index}"></a>
-      </li>`
-      str += content
+function render(completed = '') {
+  let content = ""
+  data.forEach(function(item) {
+    if (completed == "" || String(item.completed) == completed) {
+      content += liTemplate(item)
     }
-   }
+  })
 
- })
- list.innerHTML = str
+ list.innerHTML = content
  todoThing();
 }
+
 render()
 
 
-
-
-
 // 新增待辦事項
-btn_add.addEventListener('click', function(e){
-  if(thing.value.trim()==""){
+btnAdd.addEventListener('click', function(e) {
+  if (thing.value.trim() == "") {
     alert("請輸入待辦事項")
     return
   }
-  let obj = {
-    content: "",
-    status: "pending"
-  }
-  obj.content = thing.value
-  data.push(obj)
+
+  data.push({
+    id: new Date().getTime(),
+    content: thing.value,
+    completed: false 
+  })
   thing.value = ""
   render();
   todoThing();
@@ -69,22 +45,20 @@ btn_add.addEventListener('click', function(e){
 
 // 勾選已完成事項，賦予 complete
 list.addEventListener("click", function(e){
-  if(e.target.nodeName == "INPUT"){
-    let num = e.target.getAttribute("data-thing")
-    if (data[num].status == "complete"){
-      data[num].status = "pending"
-    }else {
-      data[num].status = "complete"
-    }
-  }
+  let clickedId = e.target.getAttribute("data-id")
+  let clickedItem = data.find((item) => item.id == clickedId)
+  clickedItem.completed = !clickedItem.completed
+
   todoThing();
 })
 
 // 刪除事項
 list.addEventListener("click",function(e){
-  if (e.target.nodeName == "A"){
-    let num =  e.target.getAttribute("data-thing")
-    delete data[num]
+  if (e.target.nodeName == "A") {
+    let selectedIitem = data.find((item) => item.id == e.target.getAttribute("data-id"))
+    let idx = data.indexOf(selectedIitem)
+
+    delete data[idx]
     render()
   }
 })
@@ -95,35 +69,40 @@ filterBtn.addEventListener("click", function(e){
   if (e.target.className == 'tab'){
     return
   }
-  render(e.target.className)
+  render(e.target.getAttribute("data-completed"))
 })
-    
+
+filterBtn.addEventListener("click", function(e){
+  if (e.target.nodeName == "LI"){
+    let items = filterBtn.getElementsByTagName("li")
+    for (let i = 0; i < items.length; i++) {
+      items[i].classList.remove("selected");
+    }
+    e.target.classList.add("selected")
+  }
+})
+
 
 // 待完成項目數量顯示
-
 function todoThing() {
   let count = 0;
   for (let i = 0; i < data.length; i++) {
-    if (data[i].status === "pending") {
+    if (!data[i].completed) {
       count += 1;
     }
   }
   let content = `<p>${count}個待完成項目</p>
   <a href="#">清除已完成項目</a>`;
-  list_footer.innerHTML = content;
+  listFooter.innerHTML = content;
 }
 
-
-
-
 //清除所有已完成項目
-
-list_footer.addEventListener("click", function(e){
-  if (e.target.nodeName!=="A"){
+listFooter.addEventListener("click", function(e){
+  if (e.target.nodeName !== "A") {
     return
-  }else {
+  } else {
     for (let i = data.length - 1; i >= 0; i--) {
-      if (data[i].status === "complete") {
+      if (data[i].completed) {
         data.splice(i, 1); 
       }
     }
@@ -131,6 +110,13 @@ list_footer.addEventListener("click", function(e){
   render()
 })
 
-
-
-
+// List Item 的模板
+function liTemplate (item) {
+  return `<li>
+    <label class="checkbox  for="">
+      <input class="checkBtn" type="checkbox" ${item.completed ? 'checked' : '' } data-id="${item.id}"/>
+      <span>${item.content}</span>
+    </label>
+    <a href="#" class="delete" data-id="${item.id}"></a>
+  </li>`
+}
